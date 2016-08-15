a0d61fd04745813422533c7d3f74f4414b9b2e92
Rupal Khilari <t_khilr@SFOC02JV0C3DKQ5.local>
2016-08-15 09:59:57 -0700

    Added json out. removed debug statements

--- src/gslc/jsonAssembly.fs
+++ src/gslc/jsonAssembly.fs
@@ -9,10 +9,64 @@ open Amyris.Bio.utils
 open parseTypes
 open shared
 open Newtonsoft.Json
+open System.Collections.Generic
 
-/// Emit JSON format
+/// Represents one piece of DNA for assembly, capturing its origins and relevant details
+type DNASliceJson =
+   {id: string; 
+    extId: string; 
+    dna: string;  
+    sourceChr: string; 
+    sourceFr: string; 
+    sourceTo: string; 
+    sourceFwd: bool;
+    destFr: string; 
+    destTo: string;
+    destFwd: bool; 
+    amplified: bool; 
+    sliceName: string;
+    sliceType: string;
+    breed: string;
+    description: string ; 
+}
+
+type AssemblyOutJson = 
+    { id: string;
+    name: string;
+    dnaSlices: DNASliceJson list;
+}
+
+///  Emit JSON format
 ///  tag: string  prefix for files  assemblies : List of AssemblyOut
 let dumpJsonAssemblies (prefix:string) (assemblies : AssemblyOut list) =
+    (*let debugFileName = ["/tmp/"; "debug.json"] |> String.concat ""
+    use outF = new StreamWriter(debugFileName)
+    let s:string = Newtonsoft.Json.JsonConvert.SerializeObject(assemblies,Formatting.Indented);
+    outF.WriteLine(s) *)
+
     use outF = new StreamWriter(prefix)
-    let s:string = assemblies |> Newtonsoft.Json.JsonConvert.SerializeObject
-    outF.WriteLine(s)
+    let assemblyHash =
+        assemblies 
+            |> List.map(fun a ->
+            { id = a.id.Value.ToString(); name = a.name.ToString();
+            dnaSlices = (a.dnaParts |> List.map(fun d ->
+            { id = (match d.id with | None -> "0" | Some v -> d.id.Value.ToString());
+            extId = "";
+            dna = String.Join("", d.dna);
+            sourceChr = d.sourceChr;
+            sourceFr = d.sourceFr.ToString();
+            sourceTo = d.sourceTo.ToString();
+            sourceFwd = d.sourceFwd;
+            destFr = d.destFr.ToString();
+            destTo = d.destTo.ToString();
+            destFwd = d.destFwd;
+            amplified = d.amplified;
+            sliceName = d.sliceName.ToString();
+            sliceType = d.sliceType.ToString();
+            breed = d.breed.ToString();
+            description = d.description.ToString()}));
+            })
+    
+    let newstring:string = Newtonsoft.Json.JsonConvert.SerializeObject(assemblyHash, Formatting.Indented)
+    outF.WriteLine(newstring)
+
